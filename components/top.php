bed235eb5e6beeeabcb0f4fe542b201b84c7939f
AAAR Salmon <TenzikuJohn@gmail.com>
2020-10-19 16:21:05 +0900

    ロゴにalt属性を追加

--- components/top.php
+++ components/top.php
@@ -1,5 +1,5 @@
 <section id="top">
-	<img src="/resources/logo.svg" id="logo">
+	<img src="/resources/logo.svg" id="logo" alt="ページロゴ">
 	<p id="pagetitle">AAAR Salmon</p>
 	<p id="description">しがない学生、終に鮭のホームページです。</p>
 </section>
